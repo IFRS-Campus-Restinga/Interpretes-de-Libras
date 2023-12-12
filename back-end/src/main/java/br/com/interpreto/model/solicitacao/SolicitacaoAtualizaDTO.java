46c2eb08e188a7c49629a3ff922c507bd99c1469
Pedro <2020004548@restinga.ifrs.edu.br>
2023-12-12 18:25:48 -0300

    Adicao das validacoes Usuario e Solicitacao

--- back-end/src/main/java/br/com/interpreto/model/solicitacao/SolicitacaoAtualizaDTO.java
+++ back-end/src/main/java/br/com/interpreto/model/solicitacao/SolicitacaoAtualizaDTO.java
@@ -6,6 +6,10 @@ import br.com.interpreto.model.enums.Regiao;
 import br.com.interpreto.model.enums.StatusSolicitacao;
 import br.com.interpreto.model.interprete.Interprete;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -15,14 +19,20 @@ public record SolicitacaoAtualizaDTO(
         Interprete interprete,
         Set<Regiao> regioes,
         Set<Especialidade> especialidade,
+        @Future
         @JsonFormat(pattern="yyyy-MM-dd")
         LocalDate dataEncontro,
         @JsonFormat(pattern="HH:mm")
         LocalTime horaEncontro,
+        @Positive
+        @DecimalMax(value = "9")
         int duracaoAtendimento,
         StatusSolicitacao statusSolicitacao,
+        @Size(max = 254)
         String observacaoSolicitacao,
+        @Positive(message = "Nota Surdo precisa ser maior que 0")
         Double notaSurdo,
+        @Positive(message = "Nota Surdo precisa ser maior que 0")
         Double notaInterprete,
         Endereco endereco
 ) {
--- back-end/src/main/java/br/com/interpreto/model/solicitacao/SolicitacaoCadastroDTO.java
+++ back-end/src/main/java/br/com/interpreto/model/solicitacao/SolicitacaoCadastroDTO.java
@@ -6,22 +6,31 @@ import br.com.interpreto.model.enums.Regiao;
 import br.com.interpreto.model.interprete.Interprete;
 import br.com.interpreto.model.surdo.Surdo;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Set;
 
 public record SolicitacaoCadastroDTO(
-
+        @Positive
+        @DecimalMax(value = "9")
         int duracaoAtendimento,
+        @NotNull
         Surdo surdo,
         Interprete interprete,
         Set<Regiao> regioes,
         Set<Especialidade> especialidades,
+        @NotNull
+        @Future
         @JsonFormat(pattern="yyyy-MM-dd")
         LocalDate dataEncontro,
+        @NotNull
         @JsonFormat(pattern="HH:mm")
         LocalTime horaEncontro,
+        @Size(max = 254)
         String observacaoSolicitacao,
+        @NotNull
         Endereco endereco
 
 ) {
